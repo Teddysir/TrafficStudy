723652dc97ee93df45fc733b6377f5d5ed5899d2
Teddysir <sdzx0719@naver.com>
2025-01-13 16:00:16 +0900

    feat: Entity 설계

---
+++ src/main/java/programming/study/traffictestproject/entity/PostEntity.java
@@ -0,0 +1,27 @@
+package programming.study.traffictestproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+
+}
---
+++ src/main/java/programming/study/traffictestproject/entity/UserEntity.java
@@ -0,0 +1,29 @@
+package programming.study.traffictestproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+}
