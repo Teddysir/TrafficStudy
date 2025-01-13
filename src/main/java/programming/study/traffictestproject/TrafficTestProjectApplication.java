41cb3cf592cfcebd1f861e04831c3b315fe6d254
Teddysir <sdzx0719@naver.com>
2025-01-13 15:12:20 +0900

    style: remote github

---
+++ src/main/java/programming/study/traffictestproject/TrafficTestProjectApplication.java
@@ -0,0 +1,13 @@
+package programming.study.traffictestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/programming/study/traffictestproject/TrafficTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package programming.study.traffictestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
