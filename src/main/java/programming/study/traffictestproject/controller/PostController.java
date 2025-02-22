1433b0014e522793a232ad592d59fe165418951a
Teddysir <sdzx0719@naver.com>
2025-02-22 23:14:48 +0900

    feat: N+1 문제 테스트를 위한 Entity 추가

--- src/main/java/programming/study/traffictestproject/controller/PostController.java
+++ src/main/java/programming/study/traffictestproject/controller/PostController.java
@@ -26,7 +26,13 @@ public class PostController {
     @GetMapping("/get/api/v1")
     public ResponsePostListDto getPost_1() {
         ResponseEntity.ok("v1 방식으로 게시물이 호출되었습니다. traffic 속도를 확인해주세요.");
+        System.out.println("얍얍얍!");
         return postService.getPost_1();
     }
+
+    // TODO : 처리량 / 응답시간을 개선해야한다.
+    // TODO : 기존에 많이 생각 안하고 썻던 N+1 현상을 개선하기 - 1
+    // TODO : 핫 게시물, RT 사용시 캐싱 처리를 통한 성능 개선 - 2
+    // TODO : Put / Delete 요청 한개씩 할경우 몇개부터 Put 요청이 유의미 할까? - 3
 
 }
--- src/main/java/programming/study/traffictestproject/entity/UserEntity.java
+++ src/main/java/programming/study/traffictestproject/entity/UserEntity.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -26,4 +29,11 @@ public class UserEntity {
     @Column(nullable = false)
     private String password;
 
+    // Mapping Entity
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL) // Casecade를 만약 사용자가 없어지면 Null이면 (알 수 없음) 고려해도 되겟다.
+    private List<PostEntity> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<CommentEntity> comments = new ArrayList<>();
 }
